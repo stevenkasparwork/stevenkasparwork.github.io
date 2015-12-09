6e242ccbad93a1dd4b3382c49df00bb516fefb35
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-09 13:55:47 -0600

    change 2

--- service_workers/indexed_db.js
+++ service_workers/indexed_db.js
@@ -1,6 +1,6 @@
 
 const DB_NAME = 'helix-ofsc-mobility';
-const DB_VERSION = 2; // Use a long long for this value (don't use a float)
+const DB_VERSION = 1; // Use a long long for this value (don't use a float)
 const DB_STORE_NAME = 'resources';
 const OFSC_API_KEY = 'bWV3dWRVcnVEZmpXb3E0Y0ZFUUZtSUgzWk5NM0pVUEdIY3VUaVdiRmE3aFdxRkM4N24va3BBeWJFeGRCcjZ0Zg==';
 var db;
@@ -22,7 +22,7 @@ var db;
 
         req.onupgradeneeded = function (evt) {
             console.log("openDb.onupgradeneeded");
-            var store = evt.currentTarget.result.createObjectStore(DB_STORE_NAME, { keyPath: 'db_id', autoIncrement: true });
+            var store = evt.currentTarget.result.createObjectStore(DB_STORE_NAME, { keyPath: 'id', autoIncrement: true });
 
             store.createIndex('external_id', 'external_id', { unique: true });
             store.createIndex('name', 'name', { unique: false });
@@ -73,9 +73,12 @@ var db;
         url: "//li617-242.members.linode.com/cgi-bin/controllers/getResources.php",
         data: {
             api_key: OFSC_API_KEY
-        }
+        },
+        type: 'POST'
     }).success(function(data) {
         console.log(data);
+    }).error(function(error){
+        console.log(error);
     });
 
   openDb();
