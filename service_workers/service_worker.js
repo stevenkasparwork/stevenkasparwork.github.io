58223786c642fd886991d15c7e8f342781f30931
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-09 10:31:19 -0600

    change 2

--- service_workers/service_worker.js
+++ service_workers/service_worker.js
@@ -32,18 +32,10 @@
       );
     });
 
-    this.addEventListener('fetch', function(event) {
-      var response;
-      event.respondWith(caches.match(event.request).catch(function() {
-        return fetch(event.request);
-      }).then(function(r) {
-        response = r;
-        caches.open('v1').then(function(cache) {
-          cache.put(event.request, response);
-        });
-        return response.clone();
-      }).catch(function() {
-        return caches.match('/sw-test/gallery/myLittleVader.jpg');
-      }));
-    });
+if(counter){
+    counter++;
+}
+else {
+    var counter = 0;
+}
     
