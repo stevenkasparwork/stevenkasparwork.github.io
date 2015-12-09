8fea251b721ae73204c44ce0bfee6a02a5f295bb
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-09 10:53:02 -0600

    change 2

--- service_workers/service_worker.js
+++ service_workers/service_worker.js
@@ -32,8 +32,19 @@
       );
     });
 
-navigator.serviceWorker.controller.postMessage({test: 'test message body'});
+// In the following line, you should include the prefixes of implementations you want to test.
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+// DON'T use "var indexedDB = ..." if you're not in a function.
+// Moreover, you may need references to some window.IDB* objects:
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
 
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+}
+// Let us open our database
+var request = window.indexedDB.open("MyTestDatabase", 1);
 
 
     if(counter){
