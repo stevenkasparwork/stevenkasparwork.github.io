53b65917f08b060bc177dc805faa8359fc38f52e
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-09 13:02:50 -0600

    change 2

--- service_workers/service_worker.js
+++ service_workers/service_worker.js
@@ -34,5 +34,20 @@
       );
     });
 
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v2').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+  }));
+});
+
 
 
