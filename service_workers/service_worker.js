93cad53f6643388322e21e7b3ed24631402ad8b1
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-09 13:07:16 -0600

    change 2

--- service_workers/service_worker.js
+++ service_workers/service_worker.js
@@ -1,38 +1,38 @@
 
         // register service worker
 
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service_workers/service_worker.js', { scope: '/service_workers/'}).then(function(reg) {
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service_workers/service_worker.js').then(function(reg) {
 
-        if(reg.installing) {
-          console.log('Service worker installing');
-        } else if(reg.waiting) {
-          console.log('Service worker installed');
-        } else if(reg.active) {
-          console.log('Service worker active');
-        }
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
 
-      }).catch(function(error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
-      });
-    };
-        
-        
-    this.addEventListener('install', function(event) {
-        console.log('install event');
-      event.waitUntil(
-        caches.open('v2').then(function(cache) {
-            console.log(cache);
-          return cache.addAll([
-              '/service_workers/',
-              '/service_workers/index.html',
-              '/service_workers/page2.html',
-              '/service_workers/indexed_db.js'
-          ]);
-        })
-      );
-    });
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+};
+
+
+this.addEventListener('install', function(event) {
+    console.log('install event');
+  event.waitUntil(
+    caches.open('v2').then(function(cache) {
+        console.log(cache);
+      return cache.addAll([
+          '/service_workers/',
+          '/service_workers/index.html',
+          '/service_workers/page2.html',
+          '/service_workers/indexed_db.js'
+      ]);
+    })
+  );
+});
 
 this.addEventListener('fetch', function(event) {
   var response;
