7549a416b55a62246f8516b7fcad0f1bef7d1cb5
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-15 15:54:59 -0600

    qc

--- service_workers/indexed_db.js
+++ service_workers/indexed_db.js
@@ -168,7 +168,7 @@ function getActivities(){
             state: 'Tennessee',
             time_from: '08:00:00',
             time_to: '11:00:00',
-            status: 'complete'
+            status: 'completed'
         },{
             id: '1',
             date: date_string,
