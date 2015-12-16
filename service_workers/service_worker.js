54af4b93572d24740fcaed80cfc67d91419647fd
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-16 15:36:32 -0600

    qc

--- service_workers/service_worker.js
+++ service_workers/service_worker.js
@@ -41,20 +41,23 @@ this.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
-                console.log('Found response in cache:', response);
-
+                //console.log('Found response in cache:', response);
                 return response;
+                
             }
+            
             console.log('No response found in cache. About to fetch from network...');
 
             return fetch(event.request).then(function(response) {
+                
                 console.log('Response from network is:', response);
-
                 return response;
+                
             }).catch(function(error) {
-                console.error('Fetching failed:', error);
+                
+                console.warn('Fetching failed:', error);
+                console.warn('Need to access IndexedDB');
 
-                throw error;
             });
         })
     );
