3d2e0ed0bfc4758735aa93f73b8d2dc9aec8907d
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-16 15:30:47 -0600

    qc

--- service_workers/service_worker.js
+++ service_workers/service_worker.js
@@ -36,23 +36,28 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-    console.log('fetch (service_worker)');
-   
-    event.respondWith(caches.match(event.request).catch(function(e) {
-        console.log(e);
-        return fetch(event.request);
-    }).then(function(response) {
-        console.log(response);
-        if(response){
-            caches.open('v1').then(function(cache) {
-                cache.put(event.request, response);
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+            console.log('No response found in cache. About to fetch from network...');
+
+            return fetch(event.request).then(function(response) {
+                console.log('Response from network is:', response);
+
+                return response;
+            }).catch(function(error) {
+                console.error('Fetching failed:', error);
+
+                throw error;
             });
-            return response.clone();
-        }
-    }).catch(function(e) {
-        console.log(e);
-        return caches.match('/service_workers/index.html');
-    }));
+        })
+    );
 });
 
 
