4840c67b8e59a07f592e9abe5dcbbd7b951a1a46
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-17 10:10:42 -0600

    qc

--- service_workers/indexed_db.js
+++ service_workers/indexed_db.js
@@ -275,7 +275,7 @@ function addObjectsToIndexedDB(store_name, obj_array){
                             // set the activity_id so that the api knows which activity to update
                             tmp_activity.activity_id = local_activity.id;
                             //remove the activity_id from the properties because the Activity API won't like that
-                            delete tmp_activity.properties.activity_id;
+                            delete tmp_activity.properties.id;
 
                             console.log(tmp_activity);
 
@@ -467,7 +467,7 @@ function updateActivity(event) {
         // set the activity_id so that the api knows which activity to update
         tmp_activity.activity_id = activity.id;
         //remove the activity_id from the properties because the Activity API won't like that
-        delete tmp_activity.properties.activity_id;
+        delete tmp_activity.properties.id;
         
         return updateActivityInOFSC(tmp_activity);
     }).then(function(response){
