15933237fbbade350a3060c50371a46aa406da76
stevenkaspar_work <stevenkaspar@helixmail.com>
2015-12-21 13:12:59 -0600

    testing event listeners

--- service_workers/indexed_db.js
+++ service_workers/indexed_db.js
@@ -29,7 +29,7 @@ window.onload = function(){
     
     /* set date selector to today */
     $('#date_selector').val( getDateTimeString().date );
-    document.getElementById('date_selector').addEventListener('onchange', function(event){
+    document.getElementById('date_selector').addEventListener('oninput', function(event){
         console.log('date changed');
     });
 };
